129d0fb0ad1c6a20b4f33a7b9d6f13f743205f57
pranshubarar1 <pranshubarar1851996@gmail.com>
2024-11-05 15:32:48 +0530

    Do correction in runDockerContainer.groovy by replacing command for 'building image' with 'running container'

--- vars/runDockerContainer.groovy
+++ vars/runDockerContainer.groovy
@@ -1,4 +1,4 @@
 def call(String imageName){
 	echo "Deploying docker container with image named: ${imageName}"
-	sh "docker run ${imageName} -p 8081:8081"
+	sh "docker run -p 8081:8081 ${imageName}"
 }
